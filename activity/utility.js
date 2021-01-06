c30f707034d9102debf07fa9c893bc2ff1f60119
aakash4069 <aakash4069@gmail.com>
2021-01-06 20:42:38 +0530

    utility

--- activity/utility.js
+++ activity/utility.js
@@ -2,7 +2,7 @@ module.exports.types={
     Audio:['aif','cda','mid','mp3','mpa','ogg','wav','wma','wpl'],
     Compressed:[ '7z', 'arj', 'deb', 'pkg', 'rar', 'rpm', 'tar.gz', 'z', 'zip'],
     Dis:[ 'bin', 'dmg', 'ios', 'toast', 'vcd'],
-    Datebase:[ 'csv', 'dat', 'db', 'log', 'mdb', '  sav ', '  sql ', '  tar ', '  xml '],
+    Datebase:[ 'csv', 'dat', 'db', 'log', 'mdb', 'sav', 'sql', 'tar', 'xml'],
     Email:[ 'email', 'eml', 'emlx', 'msg', 'oft', 'ost', 'pst', 'vcf'],
     Executable:[ 'apk', 'bat', 'bin', 'cgi', 'com', 'exe', 'gadget', 'jar', 'msi', 'py', 'wsf'],
     Image:[ 'ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'ps', 'psd', 'svg', 'tif', 'tiff'],
@@ -11,6 +11,5 @@ module.exports.types={
     Video:[ '3g2', '3gp', 'avi', 'flv', 'h264', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv' ],
     Documents:[ 'doc', 'docx', 'odt', 'pdf', 'rtf', 'tex', 'txt', 'wpd', 'ods', 'xls', 'xlsm', 'xlsx' ],
     Webbase:[ 'asp', 'aspx', 'cer', 'cfm', 'cgi', 'pl', 'css', 'htm', 'html', 'js', 'jsp', 'part', 'php', 'rss', 'xhtml']
-    
 
 }
